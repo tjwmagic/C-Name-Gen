22b070922b1b94d18246915fb988eb15e0a249a5
tjwmagic <travis@dragonhelm.net>
2017-08-06 15:58:57 -0500

    Minor edits

--- C++ Name Gen/Source.cpp
+++ C++ Name Gen/Source.cpp
@@ -5,29 +5,30 @@
 using namespace std;
 
 void main() {
-
-	string dwarfMaleNames[6] = { "Adrik", "Alberich", "Baern", "Barendd", "Flint", "Tordek" };
-
+	srand(time(0));
+	string dwarfMaleNames[6] = { "Adrik", "Gardain", "Baern", "Oskar", "Flint", "Veit" };
+	string dwarfLastNames[6] = { "Balderk", "Gorunn", "Ironfist", "Loderr", "Torunn", "Strakeln" };
+	int genderNumber = 0;
 	
-	int max, randomNumber, i;
 
-	for (i = 0; i <= 5; i = i + 1) {
-		cout << "Please input max integer: ";
-		cin >> max;
-		
-		srand(time(0));
+	int max, randomNumber, i, g; // int 'i' is for the name generator. int 'g' is for the gender generator
+	max = 5; // Max number of names to be displayed
 
+	genderNumber = (rand() % 2) + 1;
+	cout << "RANDOM GENDER: " << genderNumber << endl;
+
+
+	for (i = 0; i <= 5; i++) {
 		randomNumber = (rand() % max) + 1;
+		cout << "RANDOM NUMBER: " << randomNumber << endl;
 
-		cout << randomNumber << endl;
-
-		cout << dwarfMaleNames[randomNumber] << endl;
+		cout << "NAME: " << dwarfMaleNames[randomNumber] << endl;
 
 	}
 
 
-	system("pause");
+		system("pause");
 
 
 
-}
+	}
